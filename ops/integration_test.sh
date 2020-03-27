13a9a26c830a1181ac5e283a2edbd43409729077
Sebastian Bolanos <sebasheston@protonmail.com>
2020-03-27 15:08:54 +0000

    Remove logs from integration tests

--- ops/integration_test.sh
+++ ops/integration_test.sh
@@ -20,10 +20,5 @@ function runIntegrationTest() {
 git clone https://github.com/hashcloak/Meson-client /tmp/Meson-client || true
 cd /tmp/Meson-client && git fetch && git checkout $mesonClientTestCommit
 
-logFile=/tmp/meson-current/nonvoting/authority.log
-sudo tail -n 40 $logFile
 runIntegrationTest gor provider-0 $ETHEREUM_PK
-sudo tail -n 40 $logFile
-
 runIntegrationTest tbnb provider-1 $BINANCE_PK
-sudo tail -n 40 $logFile
