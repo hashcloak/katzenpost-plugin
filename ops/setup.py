ea5c03039f1b16967a2b3d3d82d5891a9073d6c0
Sebastian Bolanos <sebasheston@protonmail.com>
2020-04-23 09:16:40 +0100

    Saving unique key path config

--- ops/setup.py
+++ ops/setup.py
@@ -4,24 +4,34 @@ import subprocess as sp
 
 dockerApiUrl="https://hub.docker.com/v2/repositories"
 
-CONFIG = {
+DEFAULT_VALUES = {
+    "KATZEN": {
+        "AUTH": {
+            "CONTAINER": "hashcloak/authority",
+            "REPOSITORY": "https://github.com/katzenpost/authority",
+            "BRANCH": "master",
+            "DOCKERTAG": "master",
+            "GITHASH": "",
+        },
+        "SERVER" : {
+            "CONTAINER": "hashcloak/server",
+            "REPOSITORY": "https://github.com/katzenpost/server",
+            "BRANCH": "master",
+            "DOCKERTAG": "master",
+            "GITHASH": "",
+        },
+    },
+    "HASHCLOAK": {
+        "PLUGIN": {
+            "CONTAINER": "hashcloak/meson",
+            "DOCKERTAG": "master",
+            "BRANCH": "",
+            "GITHASH": "",
+        },
+        "CLIENT":{
+            "TEST_COMMIT": "master"
         }
-defaultValues = {
-    "KATZEN_AUTH_CONTAINER": "hashcloak/authority",
-    "KATZEN_AUTH_REPOSITORY": "https://github.com/katzenpost/authority",
-    "KATZEN_AUTH_BRANCH": "master",
-    "KATZEN_AUTH_DOCKERTAG": "master",
-    "KATZEN_AUTH_GITHASH": "",
-    "KATZEN_SERVER_CONTAINER": "hashcloak/server",
-    "KATZEN_SERVER_REPOSITORY": "https://github.com/katzenpost/server",
-    "KATZEN_SERVER_BRANCH": "master",
-    "KATZEN_SERVER_DOCKERTAG": "master",
-    "KATZEN_SERVER_GITHASH": "",
-    "HASHCLOAK_PLUGIN_CONTAINER": "hashcloak/meson",
-    "HASHCLOAK_PLUGIN_DOCKERTAG": "master",
-    "HASHCLOAK_PLUGIN_BRANCH": "",
-    "HASHCLOAK_PLUGIN_GITHASH": "",
-    "HASHCLOAK_CLIENT_TEST_COMMIT": "master"
+    },
 }
 
 def getRemoteGitHash(repositoryURL, branch):
@@ -36,26 +46,9 @@ def getLocalGitHash():
 
     return gitHash
 
-def getEnvironmentVariables(defaultValuesDict):
-    newConfig = {}
-    for key, value in defaultValuesDict.items():
 
-        splitKeyPath = key.split("_")
-        try:
-            value = os.environ[key]
-        except:
-            pass
-
-        if value == "":
-
-            if "GITHASH" in splitKeyPath[-1]:
-                getRemoteGitHash
-            elif
+def getListOfEnvironmentVariables(defaultValuesDict):
+    print()
 
 
-        newConfig[
-
-        print(key, value)
-
-
-getEnvironmentVariables(defaultValues)
+getListOfEnvironmentVariables(DEFAULT_VALUES)
