e263c8f30642e73254c2a52ae244b8b98dfc7d41
Sebastian Bolanos <sebasheston@protonmail.com>
2020-05-29 16:31:27 +0100

    Add namespace to push containers

--- ops/push_containers.py
+++ ops/push_containers.py
@@ -3,7 +3,10 @@ from config import setup_config
 
 CONFIG = setup_config()
 
-def push_containers():
+def main():
     for repo in CONFIG["REPOS"].values():
         run(["docker", "push", "{}:{}".format(repo["CONTAINER"], repo["NAMEDTAG"])])
         run(["docker", "push", "{}:{}".format(repo["CONTAINER"], repo["HASHTAG"])])
+
+if __name__ == "__main__":
+    main()
